55384c7c62295431189484abf6a0f43583da9df1
Fabian Luna <fabuluna@gmail.com>
2024-08-11 15:46:22 -0300

    inicio tp - creacion proyecto y clases

---
+++ src/dominio/Empleado.java
@@ -0,0 +1,18 @@
+package dominio;
+
+public class Empleado {
+	
+	// PROPIEDADES
+	int id;
+	String nombre;
+	int edad;
+	
+	// CONSTRUCTOR VACIO
+	public Empleado() {
+	}
+	
+	public Empleado(String nombre, int edad) {
+		
+	}
+
+}
